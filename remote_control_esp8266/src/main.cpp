62a3ef8ff7fba88e1d0646a2e6882b55b9cd8109
AndreClerigo Desktop <andreclerigo@oultook.com>
2021-05-13 13:27:23 +0100

    working sketch

--- remote_control_esp8266/src/main.cpp
+++ remote_control_esp8266/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
+// Setup Wifi credentials
 const char* ssid = "ASUS_2.4G";
 const char* password = "wifi1andar";
 WiFiServer server(80);
@@ -11,7 +12,7 @@ void left();
 void right();
 void stop();
 
-//Pins connected to the relays
+// Pins connected to the relays
 int relay1 = 12;
 int relay2 = 14;
 int relay3 = 5;
@@ -20,13 +21,16 @@ int relay4 = 16;
 unsigned long currentTime = millis();
 unsigned long previousTime = 0; 
 const long timeoutTime = 2000;
-String header;
-String forward_ = "off";
-String reverse_ = "off";
-String right_ = "off";
-String left_ = "off";
 
-void setup() {
+// In the idle state the car is stopped
+static String forward_ = "off";
+static String reverse_ = "off";
+static String right_ = "off";
+static String left_ = "off";
+String header;
+
+void setup() 
+{
     //Set the relay pins as output
     pinMode(relay1, OUTPUT);
     pinMode(relay2, OUTPUT);
@@ -58,11 +62,12 @@ void setup() {
     server.begin();
 }
 
-void loop() {
+void loop() 
+{
     WiFiClient client = server.available();
     if (client) 
     {
-        Serial.println("New Client.");          // print a message out in the serial port
+        Serial.println("New Client.");          // Print a message out in the serial port
         String currentLine = "";                // make a String to hold incoming data from the client
         currentTime = millis();
         previousTime = currentTime;
@@ -80,32 +85,21 @@ void loop() {
                     {
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-type:text/html");
-                        client.println("Connection: close");
-                        client.println();
+                        client.println("Connection: close\n");
                         
-                        // turns the GPIOs on and off
-                        if (header.indexOf("GET /Forward/on") >= 0) {
-                            forward_ = "on";
+                        // turns the GPIOs ON and OFF
+                        if (header.indexOf("GET /Forward/on") >= 0)
                             forward();
-                        } else if (header.indexOf("GET /Forward/off") >= 0) {
-                            forward_ = "off";
-                            stop();
-                        } else if (header.indexOf("GET /Reverse/on") >= 0) {
-                            reverse_ = "on";
+                        else if (header.indexOf("GET /Reverse/on") >= 0) 
                             reverse();
-                        } else if (header.indexOf("GET /Reverse/off") >= 0) {
-                            reverse_ = "off";
-                            stop();
-                        } else if (header.indexOf("GET /Right/on") >= 0) {
-                            right_ = "on";
+                        else if (header.indexOf("GET /Right/on") >= 0)
                             right();
-                        } else if (header.indexOf("GET /Right/off") >= 0) {
-                            right_ = "off";
-                            stop();
-                        } else if (header.indexOf("GET /Left/on") >= 0) {
-                            left_ = "on";
+                        else if (header.indexOf("GET /Left/on") >= 0)
                             left();
-                        } else if (header.indexOf("GET /Left/off") >= 0) {
+                        else if (header.indexOf("GET /Off") >= 0){
+                            forward_ = "off";
+                            reverse_ = "off";
+                            right_ = "off";
                             left_ = "off";
                             stop();
                         }
@@ -117,54 +111,49 @@ void loop() {
                         client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
                         client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
                         client.println(".button2 {background-color: #77878A;}</style></head>");
-                        client.println("<body><h1>My Robot</h1>");
+                        client.println("<body><h1>My Car Robot</h1>");
 
                         client.println("Forward");
-                        if (forward_=="off") {
+                        if (forward_=="off")
                             client.println("<p><a href=\"/Forward/on\"><button class=\"button\">ON</button></a></p>");
-                        } else {
-                            client.println("<p><a href=\"/Forward/off\"><button class=\"button button2\">OFF</button></a></p>");
-                        } 
+                        else 
+                            client.println("<p><a href=\"/Off\"><button class=\"button button2\">OFF</button></a></p>");
 
-                        client.print("Left------Right"); 
-                        if (left_=="off") {
+                        client.println("Left --------------- Right"); 
+                        if (left_ == "off")
                             client.print("<p><a href=\"/Left/on\"><button class=\"button\">ON</button></a>");
-                        } else {
-                            client.print("<p><a href=\"/Left/off\"><button class=\"button button2\">OFF</button></a>");
-                        }
+                        else 
+                            client.print("<p><a href=\"/Off\"><button class=\"button button2\">OFF</button></a>");
                         
-                        if (right_=="off") {
+                        if (right_ == "off")
                             client.println("<a href=\"/Right/on\"><button class=\"button\">ON</button></a></p>");
-                        } else {
-                            client.println("<a href=\"/Right/off\"><button class=\"button button2\">OFF</button></a></p>");
-                        }
+                        else
+                            client.println("<a href=\"/Off\"><button class=\"button button2\">OFF</button></a></p>");
                             
-                        if (reverse_=="off") {
+                        if (reverse_ == "off")
                             client.println("<p><a href=\"/Reverse/on\"><button class=\"button\">ON</button></a></p>");
-                        } else {
-                            client.println("<p><a href=\"/Reverse/off\"><button class=\"button button2\">OFF</button></a></p>");
-                        }
+                        else
+                            client.println("<p><a href=\"/Off\"><button class=\"button button2\">OFF</button></a></p>");
 
                         client.println("Reverse");
                         client.println("</body></html>");
                         client.println();
                         break;
-                    } else {
+                    } else
                         currentLine = "";
-                    }
-                } else if (c != '\r') {
+                } else if (c != '\r')
                     currentLine += c;      
-                }
             }
         }
         header = "";
         client.stop();
-        Serial.println("Client disconnected.");
-        Serial.println("");
+        Serial.println("Client disconnected.\n");
     }
 }
 
-void forward() {
+// Move both wheels in reverse
+void reverse() {
+    reverse_ = "on";            // Flag the reverse as ON
     stop();
     delay(10);
     digitalWrite(relay1, HIGH);
@@ -173,7 +162,9 @@ void forward() {
     digitalWrite(relay4, HIGH);
 }
 
-void right() {
+// Move the right wheel forward to turn Left
+void left() {                   
+    left_ = "on";               // Flag the left as ON
     stop();
     delay(10);
     digitalWrite(relay1, LOW);
@@ -182,7 +173,9 @@ void right() {
     digitalWrite(relay4, HIGH);
 }
 
-void left() {
+// Move the left wheel forward to turn Right
+void right() {
+    right_ = "on";              // Flag the right as ON
     stop();
     delay(10);
     digitalWrite(relay1, HIGH);
@@ -191,7 +184,9 @@ void left() {
     digitalWrite(relay4, LOW);
 }
 
-void reverse() {
+// Move both wheels forward
+void forward() {
+    forward_ = "on";            // Flag the forward as ON
     stop();
     delay(10);
     digitalWrite(relay1, LOW);
@@ -200,6 +195,7 @@ void reverse() {
     digitalWrite(relay4, LOW);
 }
 
+// Stop both wheels
 void stop()
 {
     digitalWrite(relay1, HIGH); 
